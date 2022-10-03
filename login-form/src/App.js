876e6086eea9c3c001548275fd2478917d95b7b2
Rengod95 <aorwn970@gmail.com>
2022-10-03 21:12:53 +0900

    test

---
+++ login-form/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginForm from "./components/LoginForm";
+import PlusButton from "./components/PlusButton";
+
+function App() {
+  return (
+     <LoginForm></LoginForm>
+      // <PlusButton></PlusButton>
+  );
+}
+
+export default App;
---
+++ login-form/src/components/UI/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className={classes.Card}>
+
+        </div>
+    );
+};
+
+export default Card;
